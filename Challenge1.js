00cad90afa98fc272aed35aaac398d09e0e5db43
fadaldrame97-sys <fadaldrame97@gmail.com>
2025-09-29 17:42:54 +0100

    Add files via upload

---
+++ Challenge1.js
@@ -0,0 +1,28 @@
+// const voirture={ maeque:"dacia",modele:"R69",année:"2018"};
+
+// let text= voirture;
+
+// console.log(voirture);
+
+
+// const voirture={ maeque:"dacia",modele:"R69",année:"1995"};
+
+// let text= voirture;
+
+// console.log(voirture.année);
+
+// const voirture={ maeque:"dacia",modele:"R69",année:"1995",couleur:"blanc"
+// };
+
+// let text= voirture;
+
+// console.log(voirture.couleur);
+
+const voirture={ maeque:"dacia",année:"1995"};
+
+let text= voirture;
+
+console.log(voirture);
+
+
+
---
+++ Challenge2.js
@@ -0,0 +1,28 @@
+// let student={ 
+//     Name:"fadal",
+//     age:26,
+//     sePresenter : function(){
+//     console.log("Bonjour, je m appelle " + this.Name +" et j  ai " + this.age)
+//     }
+// }
+// student.sePresenter()
+
+
+
+// const personne={name:"fadal",prenom:"Dramé",age:26,Coutry:"Sénégal"
+// };
+
+// for (let i in personne);
+
+// console.log(personne);
+ 
+
+let classe ={
+    etudiants:["fadal","Mamadou","moussa","soda"],
+afficherEtudiant : function(){
+for (let i in classe.etudiants)
+    console.log(classe.etudiants[i]);
+}
+}
+classe.afficherEtudiant();
+
