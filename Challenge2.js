8279da75bac69e8428fba8ac283382043832e45f
fadaldrame97-sys <fadaldrame97@gmail.com>
2025-09-30 10:47:10 +0100

    Add files via upload
    Complément

--- Challenge2.js
+++ Challenge2.js
@@ -17,12 +17,32 @@
 // console.log(personne);
  
 
-let classe ={
-    etudiants:["fadal","Mamadou","moussa","soda"],
-afficherEtudiant : function(){
-for (let i in classe.etudiants)
-    console.log(classe.etudiants[i]);
+// // let classe ={
+// //     etudiants:["fadal","Mamadou","moussa","soda"],
+// // afficherEtudiant : function(){
+// // for (let i in classe.etudiants)
+// //     console.log(classe.etudiants[i]);
+// // }
+// // }
+// classe.afficherEtudiant();
+
+
+let livres =[
+    {titre:"Sénégal mon pays",
+           auteur:"Fadal",
+           année:2025,
+    },
+    {      
+           titre:"Touba city",
+           auteur:"Drame",
+           année:2018,
+    },{
+           titre:"Riz au poisson",
+           auteur:"Fallou",
+           année:2004,
+}] 
+      for (let i =0 ; i < livres.length ; i++){
+      console.log(livres[i].titre);
+
 }
-}
-classe.afficherEtudiant();
 
